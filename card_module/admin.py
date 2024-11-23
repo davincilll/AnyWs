a4a21935af4f4df2d82c42dbbbfd7252450e495c
NSCola <3135287831@qq.com>
2024-11-24 00:00:39 +0800

    fix: 添加admin

--- card_module/admin.py
+++ card_module/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from app.common.decorator.AdminDecorator import custom_admin_decorator
+from card_module.models.WordCard import WordCard
+
+
 # Register your models here.
+@custom_admin_decorator(register_model=WordCard)
+class WordCardAdmin(admin.ModelAdmin):
+    pass
